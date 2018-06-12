5257c90a819612802bb0262e24b10e5a5399218c
Adish Padhani <adish.padhani@v.fitbit.com>
2018-06-12 16:14:15 -0700

    add theme and default hacker news colors

--- HackerNewsApp/ViewModels/Theme.swift
+++ HackerNewsApp/ViewModels/Theme.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2018 Raza Padhani. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct Theme {
+    let backgroundColor = UIColor(red: 246.0/255.0, green: 246.0/255.0, blue: 239.0/255.0, alpha: 1.0)
+    let themeColor = UIColor(red: 252.0/255.0, green: 102.0/255.0, blue: 33.0/255.0, alpha: 1.0)
+}
